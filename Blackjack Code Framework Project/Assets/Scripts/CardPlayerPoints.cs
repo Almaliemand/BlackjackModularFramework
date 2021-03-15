cbeff224ace1c548598c26536495bf3820c2802a
jc-theron <79307330+jc-theron@users.noreply.github.com>
2021-03-15 21:45:23 +0200

    Updated the code by adding a random number generator

---
+++ Blackjack Code Framework Project/Assets/Scripts/CardPlayerPoints.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+public class CardPlayerPoints : MonoBehaviour
+{   
+
+    public TextMeshProUGUI PlayerPoints; 
+    public GameObject drawButton; 
+    public void AddCardBtn()
+    {
+        PickRandomNumber(21);
+    }
+
+    public void PickRandomNumber (int maxInt)
+    {
+        int randomNum = Random.Range(1, maxInt+1); 
+        PlayerPoints.text = randomNum.ToString(); 
+    }
+}
