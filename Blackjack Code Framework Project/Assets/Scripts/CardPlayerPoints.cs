471004b5cecfc248fd56d01c0c03cb363cff1e0b
jc-theron <79307330+jc-theron@users.noreply.github.com>
2021-03-15 22:20:01 +0200

    Added better number choices

--- Blackjack Code Framework Project/Assets/Scripts/CardPlayerPoints.cs
+++ Blackjack Code Framework Project/Assets/Scripts/CardPlayerPoints.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 using TMPro; 
 public class CardPlayerPoints : MonoBehaviour
 {   
+    // This is to make the UI element showing the player's score accessible in the editor.
 
     public TextMeshProUGUI PlayerPoints; 
+    
+    // This button appears at the start of the game to allow players to draw their cards. Disappears after being pressed, gives random value between 1 - 21.
     public GameObject drawButton; 
-    public void AddCardBtn()
+    
+    // This button appears after draw button is pressed. Adds a random number to the total. 
+    public GameObject addButton;
+
+    // The function to be bound to the draw button. 
+    public void DrawCardBtn()
     {
         PickRandomNumber(21);
+        drawButton.SetActive (false); 
+        addButton.SetActive (true); 
     }
 
-    public void PickRandomNumber (int maxInt)
+    private void PickRandomNumber (int maxInt)
     {
         int randomNum = Random.Range(1, maxInt+1); 
         PlayerPoints.text = randomNum.ToString(); 
